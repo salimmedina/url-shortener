fd0a6fa341b1ab368daddd26b1968d786327848a
Salim Medina Carvacho <salimmedinacarvacho@MacBook-Pro-de-Salim.local>
2024-05-26 17:43:11 -0400

    feat: implement boilerplate and business logic for shortene url api

--- src/main/java/com/shortify/urlshortener/model/ShortenedUrl.java
+++ src/main/java/com/shortify/urlshortener/model/ShortenedUrl.java
@@ -1,4 +1,29 @@
 package com.shortify.urlshortener.model;
 
-public record ShortenedUrl() {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
+@Entity
+public record ShortenedUrl(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        Long id,
+        String longURL,
+        String shortURLId
+) {
+        public ShortenedUrl{
+                if (shortURLId() == null){
+                        shortURLId = UUID.randomUUID().toString();
+                }
+
+        }
+
+        public static ShortenedUrl of(String longUrl){
+                return new ShortenedUrl(null,longUrl,null);
+        }
 }
+
--- src/main/java/com/shortify/urlshortener/service/UrlShorterService.java
+++ src/main/java/com/shortify/urlshortener/service/UrlShorterService.java
@@ -1,4 +1,41 @@
 package com.shortify.urlshortener.service;
 
+import com.shortify.urlshortener.model.ShortenedUrl;
+import com.shortify.urlshortener.repository.UrlShorterRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class UrlShorterService {
+
+    private final UrlShorterRepository urlShorterRepository;
+
+    @Value("${urlshortener.baseshorturlpath}")
+    public String BASE_SHORT_URL_PATH;
+
+
+    public UrlShorterService(UrlShorterRepository urlShorterRepository) {
+        this.urlShorterRepository = urlShorterRepository;
+    }
+
+    public String shortenURL(String longURL) {
+        var optionalUrlShorter = urlShorterRepository.findByLongURL(longURL);
+        if (optionalUrlShorter.isPresent()) {
+            return buildShortURL(optionalUrlShorter.get().shortURLId());
+        }
+
+        var urlShorter = urlShorterRepository.save(ShortenedUrl.of(longURL));
+        return buildShortURL(urlShorter.shortURLId());
+    }
+
+    public Optional<String> getLongURL(String shortURLId) {
+        return urlShorterRepository.findByShortURLId(shortURLId)
+                .map(ShortenedUrl::longURL);
+    }
+
+    private String buildShortURL(String shortURLId) {
+        return BASE_SHORT_URL_PATH + "/" + shortURLId;
+    }
 }
